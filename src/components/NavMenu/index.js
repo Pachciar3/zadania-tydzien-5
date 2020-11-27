b9a2da984b98e132b095c8c74d9eec66b078ee17
Pachciar <pachciar3@gmail.com>
2020-11-27 16:52:56 +0100

    Add excersise to nav

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -12,7 +12,7 @@ function NavMenu({ handleClose }) {
         <li><NavLink activeClassName="selected" onClick={handleClose} exact to="/">Excersise 1 - Modal</NavLink></li>
         <li><NavLink activeClassName="selected" onClick={handleClose} to="/excersise-2">Excersise 2 - Snackbar</NavLink></li>
         <li><NavLink activeClassName="selected" onClick={handleClose} to="/excersise-3">Excersise 3 - Menu</NavLink></li>
-        <li><NavLink activeClassName="selected" onClick={handleClose} to="/users">Excersise 4 - Users</NavLink></li>
+        <li><NavLink activeClassName="selected" onClick={handleClose} to="/users">Excersise 4 & 5 - Users</NavLink></li>
       </ul>
     </nav>
   );
